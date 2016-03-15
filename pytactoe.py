31887ab2e50a6dd4eb49bfa76142644027ecfe16
Justin <Jtcruthers@gmail.com>
2016-03-15 14:55:58 -0400

    AI is actually pretty good

--- pytactoe.py
+++ pytactoe.py
@@ -18,17 +18,48 @@ def whoFirst():
 
         
 # Returns the index of the computer's move
-def AIMove(board):
+def AIMove(board, computerLetter, playerLetter):
     if board[4] == ' ':
         return 4
+    #Check if we have 2 spots in a row
+    canWinGame, emptySpot = determineCanWinGame(board, computerLetter)
+    if canWinGame == True:
+        return emptySpot
+    #Check if they have 2 spots in a row
+    canLoseGame, emptySpot = determineCanWinGame(board, playerLetter)
+    if canLoseGame == True:
+        return emptySpot
     isNonCornerOpen, spot = nonCornerIsOpen(board)
     if isNonCornerOpen == True:
         return spot
-    goodMove = False
-    while goodMove == False:
+
+    #Loop until get an empty spot
+    while True:
         move = random.randrange(9)
         if board[move] == ' ':
             return move
+
+#Checks to see if there are two spots filled with AI letter with the third spot empty. If so, it returns the empty spot
+def determineCanWinGame(board, letter):
+    indices = [(0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (6, 4, 2), (0, 4, 8)]
+    for indexSet in indices:
+        canWinCombo, emptySpot = canWinWithCombo(indexSet, board, letter)
+        if canWinCombo == True:
+            return True, indexSet[emptySpot] 
+
+    return False, -1
+
+def canWinWithCombo(indices, board, letter):
+    combo = board[indices[0]], board[indices[1]], board[indices[2]]
+    if combo[0] == letter and combo[1] == letter and combo[2] == ' ':
+        return True, 2
+    if combo[1] == letter and combo[2] == letter and combo[0] == ' ':
+        return True, 0
+    if combo[0] == letter and combo[2] == letter and combo[1] == ' ':
+        return True, 1
+
+    else:
+        return False, -1
 
 # If a non-corner, non-middle spot is open, it returns true and the index
 def nonCornerIsOpen(board):
@@ -107,7 +138,7 @@ def play():
 
     while True:
         print("Computer's move")
-        AIMoveIndex = AIMove(board)
+        AIMoveIndex = AIMove(board, computerLetter, userLetter)
         board[AIMoveIndex] = computerLetter
         printBoard(board)
 
@@ -129,7 +160,6 @@ def play():
             printWinner()
             break
 
-        
 
 print("Welcome to Tic-Tac-Toe!\n")
 play()
